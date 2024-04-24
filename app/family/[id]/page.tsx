38f6a1c3bf7ab9f9a6ccde5dab0796cd92123607
Kiyumars <p.maessoumi+github@gmail.com>
2024-04-25 09:12:58 +1000

    fetch relationships

--- app/family/[id]/page.tsx
+++ app/family/[id]/page.tsx
@@ -1,29 +1,36 @@
+import { fetchFamilyTree } from "@/app/actions"
 import { createClient, getSSRUser } from "@/utils/supabase/server"
 import { redirect } from "next/navigation"
 
-export default async function TreePage({ params }: { params: { id: string } }) {
+export default async function TreePage({ params }: { params: { id: number } }) {
   const user = await getSSRUser()
   if (!user) {
     redirect("/")
   }
   const client = createClient()
-  const { data: familyMembers } = await client
-    .from("family_members")
-    .select()
-    .eq("family_id", params.id)
+  const { familyMembers, familyRelationships } = await fetchFamilyTree(
+    client,
+    params.id
+  )
 
-  if (!familyMembers || !familyMembers.length) {
+  if (
+    !familyMembers ||
+    !familyMembers.length ||
+    !familyRelationships ||
+    !familyRelationships?.length
+  ) {
     return (
       <div>
         <h1>No family members in this family</h1>
       </div>
     )
   }
+
   return (
     <div>
       <ul>
         {familyMembers.map((member) => {
-          return <li>{JSON.stringify(member)}</li>
+          return <li key={member.uuid}>{JSON.stringify(member)}</li>
         })}
       </ul>
     </div>
