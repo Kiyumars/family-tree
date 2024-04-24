e6da4706cf260ca63a64b11be8db7bacfd7e32dd
Kiyumars <p.maessoumi+github@gmail.com>
2024-04-24 21:19:37 +1000

    display families from db

--- app/family/[id]/page.tsx
+++ app/family/[id]/page.tsx
@@ -1,15 +1,31 @@
-import { getSSRUser } from "@/utils/supabase/server"
+import { createClient, getSSRUser } from "@/utils/supabase/server"
 import { redirect } from "next/navigation"
-import * as React from "react"
 
-export default async function Tree() {
+export default async function TreePage({ params }: { params: { id: string } }) {
   const user = await getSSRUser()
   if (!user) {
     redirect("/")
   }
+  const client = createClient()
+  const { data: familyMembers } = await client
+    .from("family_members")
+    .select()
+    .eq("family_id", params.id)
+
+  if (!familyMembers || !familyMembers.length) {
+    return (
+      <div>
+        <h1>No family members in this family</h1>
+      </div>
+    )
+  }
   return (
     <div>
-      <h1>Here is one family</h1>
+      <ul>
+        {familyMembers.map((member) => {
+          return <li>{JSON.stringify(member)}</li>
+        })}
+      </ul>
     </div>
   )
 }
