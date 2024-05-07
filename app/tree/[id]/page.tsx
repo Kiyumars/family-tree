65765a6493a40688f7f0426c160343d3a2e75ab4
Kiyumars <p.maessoumi+github@gmail.com>
2024-05-07 10:13:13 +1000

    mockup suspense states

--- app/tree/[id]/page.tsx
+++ app/tree/[id]/page.tsx
@@ -2,13 +2,22 @@ import { getSSRUser } from "@/utils/supabase/server"
 import { redirect } from "next/navigation"
 import { fetchTreeData } from "../../actions"
 import Members, { Relationship } from "../components/Members"
+import { Suspense } from "react"
 
 export default async function TreePage({ params }: { params: { id: number } }) {
   const user = await getSSRUser()
   if (!user) {
     redirect("/")
   }
-  const { membersRes, relationshipsRes } = await fetchTreeData(params.id)
+  return (
+    <Suspense>
+      <Content id={params.id} />
+    </Suspense>
+  )
+}
+
+async function Content({ id }: { id: number }) {
+  const { membersRes, relationshipsRes } = await fetchTreeData(id)
 
   if (!membersRes.data?.length) {
     return (
@@ -17,14 +26,11 @@ export default async function TreePage({ params }: { params: { id: number } }) {
       </div>
     )
   }
-
   return (
-    <div>
-      <Members
-        members={membersRes.data}
-        // ts does not understand that relationship_type can not be null
-        relationships={relationshipsRes.data as unknown as Relationship[]}
-      />
-    </div>
+    <Members
+      members={membersRes.data}
+      // ts does not understand that relationship_type can not be null
+      relationships={relationshipsRes.data as unknown as Relationship[]}
+    />
   )
 }
--- app/tree/page.tsx
+++ app/tree/page.tsx
@@ -1,12 +1,22 @@
 import { createClient, getSSRUser } from "@/utils/supabase/server"
 import Link from "next/link"
 import { redirect } from "next/navigation"
+import { Suspense } from "react"
 
 export default async function Trees() {
   const user = await getSSRUser()
   if (!user) {
     redirect("/")
   }
+
+  return (
+    <Suspense>
+      <Content />
+    </Suspense>
+  )
+}
+
+async function Content() {
   const client = createClient()
   const { data: tree } = await client.from("trees").select()
   if (!tree || !tree.length) {
